1c633ccda2115296b34460f9cc5fe98b11b57d94
gabibonatto <gabibonattomarcon@gmail.com>
2024-03-25 10:14:20 -0300

    Lista01

---
+++ Exercicio1.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Exercicio1 {
+    public static void executar () {
+        Scanner scanner = new Scanner(System.in);
+        
+        System.out.print("Digite um número: ");
+        int numero = scanner.nextInt();
+        
+        if(numero > 10) {
+            System.out.println("Número maior do que 10!");
+        } else {
+            System.out.println("Número menor ou igual a 10!");
+        }
+        
+        scanner.close();
+    }
+}
